41cafbe5ebac5952dbfcafe0c77eb80505701c5f
DANNY <dannyggg23@gmail.com>
2019-12-10 17:04:31 -0500

    logo municipio

---
+++ vistas/index.php
@@ -0,0 +1,160 @@
+<?php
+
+require 'header.php';
+
+?>
+
+        <div class="container-fluid">
+
+        <div class="row" align="center">
+            <div class="col-md-12 col-md-offset-2 col-xs-12">
+                <img width="20%" src="../public/img/logoMunicipalidad.png" class="img-responsive">
+            </div>
+        </div>
+        <br>
+
+        <div class="row">
+                    <!-- Column -->
+                    <div class="col-sm-12 col-lg-4">
+                        <div class="card">
+                            <div class="card-body text-center">
+                                <div class="profile-pic m-b-20 m-t-20">
+                                    <img src="../../assets/images/users/5.jpg" width="150" class="rounded-circle" alt="user">
+                                    <h4 class="m-t-20 m-b-0">Daniel Kristeen</h4>
+                                    <a href="mailto:danielkristeen@gmail.com">danielkristeen@gmail.com</a>
+                                </div>
+                                <div class="badge badge-pill badge-light font-16">Dashboard</div>
+                                <div class="badge badge-pill badge-light font-16">UI</div>
+                                <div class="badge badge-pill badge-light font-16">UX</div>
+                                <div class="badge badge-pill badge-info font-16" data-toggle="tooltip" data-placement="top" title="" data-original-title="3 more">+3</div>
+                            </div>
+                            <div class="p-25 border-top m-t-15">
+                                <div class="row text-center">
+                                    <div class="col-6 border-right">
+                                        <a href="#" class="link d-flex align-items-center justify-content-center font-medium"><i class="mdi mdi-message font-20 m-r-5"></i>Message</a>
+                                    </div>
+                                    <div class="col-6">
+                                        <a href="#" class="link d-flex align-items-center justify-content-center font-medium"><i class="mdi mdi-developer-board font-20 m-r-5"></i>Portfolio</a>
+                                    </div>
+                                </div>
+                            </div>
+                        </div>
+                    </div>
+
+
+                    <div class="col-sm-12 col-lg-4">
+                        <div class="card">
+                            <div class="card-body text-center">
+                                <div class="profile-pic m-b-20 m-t-20">
+                                    <img src="../../assets/images/users/5.jpg" width="150" class="rounded-circle" alt="user">
+                                    <h4 class="m-t-20 m-b-0">Daniel Kristeen</h4>
+                                    <a href="mailto:danielkristeen@gmail.com">danielkristeen@gmail.com</a>
+                                </div>
+                                <div class="badge badge-pill badge-light font-16">Dashboard</div>
+                                <div class="badge badge-pill badge-light font-16">UI</div>
+                                <div class="badge badge-pill badge-light font-16">UX</div>
+                                <div class="badge badge-pill badge-info font-16" data-toggle="tooltip" data-placement="top" title="" data-original-title="3 more">+3</div>
+                            </div>
+                            <div class="p-25 border-top m-t-15">
+                                <div class="row text-center">
+                                    <div class="col-6 border-right">
+                                        <a href="#" class="link d-flex align-items-center justify-content-center font-medium"><i class="mdi mdi-message font-20 m-r-5"></i>Message</a>
+                                    </div>
+                                    <div class="col-6">
+                                        <a href="#" class="link d-flex align-items-center justify-content-center font-medium"><i class="mdi mdi-developer-board font-20 m-r-5"></i>Portfolio</a>
+                                    </div>
+                                </div>
+                            </div>
+                        </div>
+                    </div>
+
+
+                    <div class="col-sm-12 col-lg-4">
+                        <div class="card">
+                            <div class="card-body text-center">
+                                <div class="profile-pic m-b-20 m-t-20">
+                                    <img src="../../assets/images/users/5.jpg" width="150" class="rounded-circle" alt="user">
+                                    <h4 class="m-t-20 m-b-0">Daniel Kristeen</h4>
+                                    <a href="mailto:danielkristeen@gmail.com">danielkristeen@gmail.com</a>
+                                </div>
+                                <div class="badge badge-pill badge-light font-16">Dashboard</div>
+                                <div class="badge badge-pill badge-light font-16">UI</div>
+                                <div class="badge badge-pill badge-light font-16">UX</div>
+                                <div class="badge badge-pill badge-info font-16" data-toggle="tooltip" data-placement="top" title="" data-original-title="3 more">+3</div>
+                            </div>
+                            <div class="p-25 border-top m-t-15">
+                                <div class="row text-center">
+                                    <div class="col-6 border-right">
+                                        <a href="#" class="link d-flex align-items-center justify-content-center font-medium"><i class="mdi mdi-message font-20 m-r-5"></i>Message</a>
+                                    </div>
+                                    <div class="col-6">
+                                        <a href="#" class="link d-flex align-items-center justify-content-center font-medium"><i class="mdi mdi-developer-board font-20 m-r-5"></i>Portfolio</a>
+                                    </div>
+                                </div>
+                            </div>
+                        </div>
+                    </div>
+        
+                </div>
+
+
+                <div class="row">
+                    <div class="col-sm-12">
+                        <div class="card">
+                            <div class="row">
+                                <div class="col-sm-12 col-lg-12 border-left">
+                                    <div class="card-body">
+                                        <ul class="list-style-none">
+                                            <li class="m-t-30">
+                                                <div class="d-flex align-items-center">
+                                                    <i class="mdi mdi-emoticon-happy display-5 text-muted"></i>
+                                                    <div class="m-l-10">
+                                                        <h5 class="m-b-0">Positive Reviews</h5>
+                                                        <span class="text-muted">25547 Reviews</span></div>
+                                                </div>
+                                                <div class="progress">
+                                                    <div class="progress-bar bg-success" role="progressbar" style="width: 47%" aria-valuenow="47" aria-valuemin="0" aria-valuemax="100"></div>
+                                                </div>
+                                            </li>
+                                            <li class="m-t-40">
+                                                <div class="d-flex align-items-center">
+                                                    <i class="mdi mdi-emoticon-sad display-5 text-muted"></i>
+                                                    <div class="m-l-10">
+                                                        <h5 class="m-b-0">Negative Reviews</h5>
+                                                        <span class="text-muted">5547 Reviews</span></div>
+                                                </div>
+                                                <div class="progress">
+                                                    <div class="progress-bar bg-orange" role="progressbar" style="width: 33%" aria-valuenow="33" aria-valuemin="0" aria-valuemax="100"></div>
+                                                </div>
+                                            </li>
+                                            <li class="m-t-40 m-b-40">
+                                                <div class="d-flex align-items-center">
+                                                    <i class="mdi mdi-emoticon-neutral display-5 text-muted"></i>
+                                                    <div class="m-l-10">
+                                                        <h5 class="m-b-0">Neutral Reviews</h5>
+                                                        <span class="text-muted">547 Reviews</span></div>
+                                                </div>
+                                                <div class="progress">
+                                                    <div class="progress-bar bg-info" role="progressbar" style="width: 20%" aria-valuenow="20" aria-valuemin="0" aria-valuemax="100"></div>
+                                                </div>
+                                            </li>
+                                        </ul>
+                                    </div>
+                                </div>
+                            </div>
+                        </div>
+                    </div>
+                </div>
+               
+
+        </div>
+<?php
+
+require 'footer.php';
+
+?>
+
+   
+      
+    
+    
